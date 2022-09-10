f1a21e226785059f6dc119f3326eec382b670570
Nickolay Trukhin <coolone.official@gmail.com>
2022-09-10 23:33:37 +0200

    Initial Commit

---
+++ mazeGame/mazeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  mazeGameApp.swift
+//  mazeGame
+//
+//  Created by Nickolay Truhin on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct mazeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
